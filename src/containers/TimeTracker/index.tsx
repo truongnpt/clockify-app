d3ebea45aecfb5a2e30ca4cdc7a7194d94d6f006
TruongNP <truongnp@fabatechnology.com>
2022-11-04 10:04:45 +0700

    fix react select

--- src/containers/TimeTracker/index.tsx
+++ src/containers/TimeTracker/index.tsx
@@ -6,10 +6,31 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Flex, H3 } from '../../components';
+import { Flex, FormControl, Span } from '../../components';
 import styled from '@emotion/styled';
+import { FormSelectNormal } from 'components/Form/FormCustomSelect';
+import { NavDropdown } from 'react-bootstrap';
+
+export function CustomLabel(data) {
+  return (
+    <Span variant="body" ml="s">
+      {data.label}
+    </Span>
+  );
+}
 
 export function TimeTracker(props: any) {
+  const listProjects = [
+    {
+      label: 'Qualee',
+      value: 'qualee',
+    },
+    {
+      label: 'Transportme',
+      value: 'Transportme',
+    },
+  ];
+
   return (
     <>
       <Helmet
@@ -17,9 +38,41 @@ export function TimeTracker(props: any) {
         defaultTitle={'Time Tracker'}
       />
       <HeaderContentWrapper>
-        <Flex alignItems="center">
-          <H3 color="dimGray">{'Time Tracker'}</H3>
-        </Flex>
+        <TitleWrapper>
+          <FormControl
+            id="title"
+            type="text"
+            placeholder={'What have you worked on?'}
+            color="dimGray"
+            value={''}
+          />
+        </TitleWrapper>
+        <ActionsWrapper>
+          <FormSelectNormal
+            select
+            width="200px"
+            className="mx-3"
+            name="projects"
+            onChange={() => {}}
+            defaultValue={null}
+            options={listProjects}
+            placeholder={'Task @Project'}
+            isSearchable={true}
+            formatOptionLabel={CustomLabel}
+          />
+          <FormSelectNormal
+            select
+            width="200px"
+            className="mx-3"
+            name="projects"
+            onChange={() => {}}
+            defaultValue={null}
+            options={listProjects}
+            placeholder={'Task @Project'}
+            isSearchable={true}
+            formatOptionLabel={CustomLabel}
+          />
+        </ActionsWrapper>
       </HeaderContentWrapper>
     </>
   );
@@ -27,4 +80,46 @@ export function TimeTracker(props: any) {
 
 const HeaderContentWrapper = styled(Flex)<any>`
   width: 100%;
+  padding: 10px 0;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid ${p => p.theme.colors.northeastSnow};
+  background-color: ${p => p.theme.colors.primaryWhite};
+`;
+
+const TitleWrapper = styled.div<any>`
+  flex: 1;
+
+  .form-control {
+    margin: 0;
+    border: none !important;
+  }
+`;
+
+const ActionsWrapper = styled(Flex)<any>`
+  & > div {
+    margin: 0;
+  }
+
+  .react-select__placeholder {
+    width: auto;
+    text-transform: capitalize;
+    color: ${p => p.theme.colors.primaryGreen};
+    font-weight: ${p => p.theme.fontWeights.semiBold};
+  }
+  .react-select__control {
+    margin: 0;
+    /* border: none !important; */
+    cursor: pointer;
+  }
+
+  .react-select__single-value {
+    color: ${p => p.theme.colors.primaryGreen};
+  }
+
+  .react-select__indicator {
+    path {
+      fill: ${p => p.theme.colors.primaryGreen};
+    }
+  }
 `;
